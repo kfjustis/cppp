1b88ba1d6ee4f5ea4499446cd71b5762683f3fae
kfjustis <kfjustis@gmail.com>
2017-05-15 23:36:24 -0500

    Added comments to IsUnique()

--- src/arrays-and-strings/stringer.cpp
+++ src/arrays-and-strings/stringer.cpp
@@ -5,8 +5,11 @@ void Stringer::PrintString(const std::string& s) {
 }
 
 bool Stringer::IsUnique(const std::string& s) {
+    // We don't care about order, so why not get average O(1) powers?
     std::unordered_map<char, int> str_map;
     
+    // If character isn't in the map, add it
+    // Otherwise, string is not unique so return false
     for (int i = 0; i < s.length(); ++i) {
         std::unordered_map<char, int>::const_iterator found = str_map.find(s[i]);
         if (found == str_map.end()) {
@@ -16,7 +19,7 @@ bool Stringer::IsUnique(const std::string& s) {
         }
     }
     
-    /* Print map for fun
+    /* Uncomment to print map as well
     std::cout << "Map contents: " << std::endl;
     for (auto item = str_map.begin(); item != str_map.end(); ++item)
         std::cout << " " << item->first << ":" << item->second << std::endl;
